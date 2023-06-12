50a4d9299312b4b942ee283326bdec3d9c4cb252
Aayam Bhattarai <aayamgt@hotmail.com>
2023-06-12 09:01:51 +0545

    Made the canvas container grid and div elements as flex

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const container = document.getElementById('container');
+const container = document.getElementById('board');
 
-function makeGrid(rows,cols) {
-  for (let c = 0; c < (rows * cols) ; c++) {
+function makeGrid(size) {
+  board.style.setProperty('--grid-rows', size); // creating css variables and storing their values
+  board.style.setProperty('--grid-cols', size); 
+  for (let c = 0; c < (size * size) ; c++) {
     let cell = document.createElement('div');
     cell.classList.add('grid-items');
     container.appendChild(cell);
   };
 };
 
-makeGrid(16, 16);
+makeGrid(16); // Single is enough as we are creating a square
