d9780a0be670302b47a9a28305daee336b08863f
Aayam Bhattarai <aayamgt@hotmail.com>
2023-06-12 11:12:57 +0545

    Added additional grid size

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ let currentSize = DEFAULT_SIZE;
 
 const container = document.getElementById('board');
 const clsBtn = document.getElementById('clear');
-const sizeBtn = document.getElementById('size'); 
+const sizeBtn = document.getElementById('size16');
+const sizeBtn2 = document.getElementById('size32')
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 clsBtn.onclick = () => clear();
-sizeBtn.onclick = () => setSize();
-
+sizeBtn.onclick = (e) => setSize(sizeBtn.value);
+sizeBtn2.onclick = (e) => setSize(sizeBtn2.value);
 
 function makeGrid(size) {
   board.style.setProperty('--grid-rows', size); // creating css variables and storing their values
@@ -37,9 +38,17 @@ function clear(){
   makeGrid(currentSize);
 }
 
-function setSize(){
-  currentSize = sizeBtn.value;
-  container.innerHTML = '';
-  makeGrid(parseInt(currentSize));
-}
+function setSize(e){
+  if(e === '16'){
+    currentSize = e;
+    container.innerHTML = '';
+    makeGrid(parseInt(e));
+  }
+  else if(e === '32'){
+    currentSize = e;
+    container.innerHTML = '';
+    makeGrid(parseInt(e));
+  }
+} 
+
 makeGrid(DEFAULT_SIZE); // Single is enough as we are creating a square
