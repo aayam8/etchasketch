6b1ca3915611411f3d03de5689ef15e95aac0289
Aayam Bhattarai <aayamgt@hotmail.com>
2023-06-12 09:33:22 +0545

    Can be drawn in the grid by hovering the mouse

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const container = document.getElementById('board');
+const container = document.getElementById('clear')
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
 
 function makeGrid(size) {
   board.style.setProperty('--grid-rows', size); // creating css variables and storing their values
@@ -6,8 +13,17 @@ function makeGrid(size) {
   for (let c = 0; c < (size * size) ; c++) {
     let cell = document.createElement('div');
     cell.classList.add('grid-items');
+    cell.addEventListener('mouseover', draw);
+    cell.addEventListener('mouseDown', draw);
     container.appendChild(cell);
   };
 };
+
+function draw(d) {
+  if (d.type === 'mouseover' && !mouseDown) {
+    return d.target.style.backgroundColor = `rgb(0,0,0)`;
+  }
+};
+
 
 makeGrid(16); // Single is enough as we are creating a square
